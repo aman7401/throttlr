bd1e61f20d88070e45fc13218b648f04b7cc5724
aman7401 <amanrai7401@gmail.com>
2026-05-03 15:21:59 +0530

    add GlobalExceptionHandler with 429 response for rate limit exceeded

---
+++ src/main/java/com/throttlr/handler/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.throttlr.handler;
+
+import com.throttlr.exception.RateLimitExceededException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RateLimitExceededException.class)
+    public ResponseEntity<Map<String, Object>> handleRateLimitExceeded(RateLimitExceededException ex) {
+        return ResponseEntity
+                .status(HttpStatus.TOO_MANY_REQUESTS)
+                .body(Map.of(
+                        "error", "Rate limit exceeded",
+                        "remaining", 0,
+                        "resetInSeconds", ex.getResult().getResetTimeSeconds()
+                ));
+    }
+}
